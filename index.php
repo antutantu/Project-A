e83001217058f7688f658b5b3c48662d4d3a2cfc
Buda Ninja <git@buda.ninja>
2020-01-29 11:48:00 +0200

    Create index.php
    no specific goal yet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Wish</title>
+</head>
+<body>
+    <div class="content">
+        <h1>Favorite Wish</h1>
+        <ul>
+            <li>I want to buy a new gaming video card</li>
+            
+        </ul>
+    </div>
+</body>
+</html> 
