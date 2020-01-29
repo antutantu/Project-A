8729f190d2b124a89991112d6bbd4f4a617f6cec
antutantu <59747976+antutantu@users.noreply.github.com>
2020-01-29 16:39:30 +0200

    addWish.php

---
+++ addWish.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Wish</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+    <h1>Favorite Wish</h1>
+        <h3>Add your favorite wish</h3>
+        <form>
+            <label for="new-wish-title">My Wish</label>
+            <input type="text" name="newWish" id="new-wish-title" placeholder="My wish is ...">
+            <input type="submit" value="Add">
+        </form>
+    </div>
+</body>
+</html> 
