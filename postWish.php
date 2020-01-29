5b638aaf38bb82813b4c8f00eeb6e0dc5a182d80
antutantu <59747976+antutantu@users.noreply.github.com>
2020-01-29 21:49:42 +0200

    if add empty

--- postWish.php
+++ postWish.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
+    if (empty($_POST['newWish'])) {
+        header('Location: /project-a/addWish.php');
+    }else{
     $Wish = $_POST['newWish'];
-    array_push($_SESSION['WishList'], $Wish);
+    array_push($_SESSION['WishList'], $Wish); 
     header('Location: /project-a');
+    }
 ?> 
