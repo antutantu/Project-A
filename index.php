00fb51d68231cf955f96db3cd987eed64a13692b
Buda Ninja <git@buda.ninja>
2020-01-30 13:10:43 +0200

    create login.php

--- index.php
+++ index.php
@@ -38,6 +38,5 @@
         <a class="button" href="clearWishes.php" button type="button" onclick="alert('Are you sure?')">Clear Wiches</button></a>
         <a class="button" href="runWishes.php" button type="button" >Run Wishes</button></a>
     </div>
-    </div>
 </body>
 </html> 
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Run Wishes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+<form method="post" action="login-DB.php">
+    <h1>Please log in</h1>
+    <input type="text" name="username" id="username" placeholder="Username">
+    <input type="text" name="password" id="password" placeholder="Password">
+    <br>
+    <br>
+    <input type="submit" value="Log In" class='submit'>
+    <br>
+    <br>
+    <a class="button" href="index.php" button type="button">Continue without Login</button></a>
+</form>
+    </div>
+</body>
+</html> 
